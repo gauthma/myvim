22f1450b02fe2ad4aea21f92da0ca395bdfa0c5d
gauthma <oscar@erroneousthoughts.org>
2018-01-28 12:06:21 +0000

    setup.sh now has no tabs, and added solarized theme for airline

--- setup.sh
+++ setup.sh
@@ -1,35 +1,39 @@
 #! /bin/bash
 
 function do_setup {
-	rm -rf ~/.vimrc ~/.vim
-	git clone https://github.com/gauthma/myvim.git ~/.vim
-	mkdir -p ~/.vim/autoload ~/.vim/bundle
-	curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-	ln -s ~/.vim/vimrc ~/.vimrc
+  rm -rf ~/.vimrc ~/.vim
+  git clone https://github.com/gauthma/myvim.git ~/.vim
+  mkdir -p ~/.vim/autoload ~/.vim/bundle
+  curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  ln -s ~/.vim/vimrc ~/.vimrc
 
-	# download plugins
-	cd
-	while read -r line ; do
-		# skip blank or empty lines
-		if [ -z "$line" -o "$line" == " " ]; then
-			continue
-		fi
-		# skip comment lines... (which start with ")
-		if [[ $line =~ ^\" ]]; then
-			continue
-		fi
-		cd ~/.vim/bundle
-		echo "Cloning $line..."
-		git clone $line > /dev/null
-	done < "~/.vim/customizations/vim_plugin_list"
+  # download plugins
+  cd
+  while read -r line ; do
+    # skip blank or empty lines
+    if [ -z "$line" -o "$line" == " " ]; then
+      continue
+    fi
+    # skip comment lines... (which start with ")
+    if [[ $line =~ ^\" ]]; then
+      continue
+    fi
+    cd ~/.vim/bundle
+    echo "Cloning $line..."
+    git clone $line > /dev/null
+  done < ".vim/customizations/vim_plugin_list"
+  # WARNING: do NOT put a tilde (~) in the above line, it will NOT work!
 }
 
 function postprocess {
-	# I just use the snippets in ~/.vim/snippets/
-	rm -rf ~/.vim/bundle/snipmate.vim/snippets
+  # I just use the snippets in ~/.vim/snippets/
+  rm -rf ~/.vim/bundle/snipmate.vim/snippets
 
-	# run :Helptags
-	vim -c "Helptags|q"
+  # solarized theme for vim-airline
+  cp ~/.vim/customizations/vim-airline-solarized-theme.vim ~/.vim/bundle/vim-airline/autoload/airline/themes/solarized.vim
+
+  # run :Helptags
+  vim -c "Helptags|q"
 }
 
 echo "WARNING! WARNING! WARNING!"
@@ -37,6 +41,6 @@ echo "This will DELETE both ~/.vimrc and ~/.vim."
 read -p "Are you sure you want to continue? [type uppercase y]" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Y]$ ]]; then
-	do_setup
-	postprocess
+  do_setup
+  postprocess
 fi
