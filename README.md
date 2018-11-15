9a999f51685f275c18b1d0d6735ac21738183bec
gauthma <oscar@erroneousthoughts.org>
2018-11-15 16:22:42 +0000

    modified setup script.
    It now does all modifications and plugin downloads inside the clone'd
    copy, and only after copies things to ~/.vim.

--- README.md
+++ README.md
@@ -5,38 +5,25 @@ The name pretty much says it all.
 Requirements: 
 --- 
 
-- The **TeX-9** plugin requires that vim be compiled with python
-	support. 
-- My overall settings I use assume a slew of external
-	programs: aspell, luatex (lualatex), bibtex, sudo, etc. These are not
-	strictly necessary to use my vim settings; they are just programs that
-	I use a lot, so they've ended up in my settings.
+- The **TeX-9** plugin requires that vim be compiled with python support. 
+- My overall settings I use assume a slew of external programs: aspell, luatex (lualatex), bibtex, sudo, etc. These are not strictly necessary to use my vim settings; they are just programs that I use a lot, so they've ended up in my settings.
 	
 Install: 
 --- 
 
 ``` bash 
-$ wget https://raw.githubusercontent.com/gauthma/myvim/master/setup.sh
-$ sh setup.sh
-$ rm setup.sh
+$ git clone https://github.com/gauthma/myvim.git myvim
+$ sh myvim/setup.sh
+$ rm -rf myvim
 ```
 
-**NOTE**: this will ***DELETE*** any existing `~/.vim` and `~/.vimrc`,
-so due care must be taken to back up any previous settings.
+**NOTE**: this will ***DELETE*** any existing `~/.vim` and `~/.vimrc`, so due care must be taken to back up any previous settings.
 
-This installs (*most*; TODO fix this) the plugins. The list of plugins I
-use can be seen in the file `~/.vim/customizations/vim_plugin_list` .
-The script also runs `pathogen`'s `:Helptags`. Which in turn runs
-`:helptags <plugin/doc>` for each directory in your runtime path.
+This installs (*most*; TODO fix this) the plugins. The list of plugins I use can be seen in the file `~/.vim/customizations/vim_plugin_list` . The script also runs `pathogen`'s `:Helptags`. Which in turn runs `:helptags <plugin/doc>` for each directory in your runtime path.
 
 And presto, it's ready to use. Feedback on improvements is always welcome. Enjoy!
 
-Information on the patches in the `customizations` folder can be
-found on my Vim Hacks page,
-[here](https://erroneousthoughts.org/writings/vim-hacks/).
-
 Notes:
 ---
- - The config I use can be found here: https://github.com/gauthma/myvim
  - IMPORTANT: vim-pandoc requires that vim be compiled with python support. Otherwise, comment it out in vimrc!
  - when using LaTeX, pressing Esc *once* will keep the insert mode indication, but it will allow you to execute some normal mode commands, in particular saving the file (:w). I'm still working through the details
--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
+GIT_CLONE_DIR=""
+
+function cd_cloned_dir {
+  GIT_CLONE_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+  echo "${GIT_CLONE_DIR}"
+}
+
 function do_setup {
-  rm -rf ~/.vimrc ~/.vim
-  git clone https://github.com/gauthma/myvim.git ~/.vim
-  mkdir -p ~/.vim/autoload ~/.vim/bundle
-  curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-  ln -s ~/.vim/vimrc ~/.gvimrc
+  echo "Downloading plugins..."
+
+  mkdir -p autoload bundle
+  curl -LSso autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
   # download plugins
-  cd
   while read -r line ; do
     # skip blank or empty lines
     if [ -z "$line" -o "$line" == " " ]; then
@@ -18,33 +23,53 @@ function do_setup {
     if [[ $line =~ ^\" ]]; then
       continue
     fi
-    cd ~/.vim/bundle
+    cd "${GIT_CLONE_DIR}"/bundle
     echo "Cloning $line..."
     git clone $line > /dev/null
-  done < ".vim/customizations/vim_plugin_list"
+    cd "${GIT_CLONE_DIR}"
+  done < "./customizations/vim_plugin_list"
   # WARNING: do NOT put a tilde (~) in the above line, it will NOT work!
-  cd
+  cd "${GIT_CLONE_DIR}"
 }
 
-function postprocess {
+function customize {
+  echo "Customizing..."
+  cd "${GIT_CLONE_DIR}"
+
   # I just use the snippets in ~/.vim/snippets/
-  rm -rf ~/.vim/bundle/snipmate.vim/snippets
+  rm -rf bundle/snipmate.vim/snippets
 
   # setup lightline.vim to my liking
-  cd ~/.vim/bundle/lightline.vim
-  patch -p1 < ~/.vim/customizations/vim-lightline-background.patch
-  cd
+  cd bundle/lightline.vim
+  patch -p1 < ../../customizations/vim-lightline-background.patch
+  cd "${GIT_CLONE_DIR}"
 
-  # run :Helptags
+  # ditto for solarized
+  cd bundle/vim-colors-solarized
+  patch -p1 < ../../customizations/vim-colors-solarized.patch
+  cd "${GIT_CLONE_DIR}"
+}
+
+function do_install {
+  echo "WARNING! WARNING! WARNING!"
+  echo "This will DELETE ~/.vimrc, ~/.vim, and ~/.gvimrc"
+  read -p "Are you sure you want to continue? [type uppercase y]" -n 1 -r
+  echo # (optional) move to a new line
+  if [[ $REPLY =~ ^[Y]$ ]]; then
+    rm -rf ~/.vimrc ~/.vim ~/.gvimrc
+    cp -r . ~/.vim
+    rm -f ~/.vim/setup.sh ~/.vim/README.md
+    ln -s ~/.vim/vimrc ~/.gvimrc
+  fi
+
+  # Run :Helptags. This requires "execute pathogen#infect()" in ~/.vimrc, 
+  # so can only be done after copying (install) stage.
   vim -c "Helptags|q"
 }
 
-cd
-echo "WARNING! WARNING! WARNING!"
-echo "This will DELETE both ~/.vimrc and ~/.vim."
-read -p "Are you sure you want to continue? [type uppercase y]" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Y]$ ]]; then
-  do_setup
-  postprocess
-fi
+cd_cloned_dir
+do_setup
+customize
+do_install
+
+echo "Setup and install complete. You might wish to remove the git clone'd folder." 
